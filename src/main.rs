c947aae4416fc46aef132a4745f688c884d0f063
TENX-S <ttenx@pm.me>
2020-11-30 17:11:20 +0800

    Released 1.0.1 ~

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::process::Command;
 use anyhow::{Result, bail};
 use std::path::{Path, PathBuf};
 use walkdir::{WalkDir, DirEntry};
-use ansi_term::Colour::{Cyan, Red, Green,};
+use ansi_term::ANSIGenericString;
+use ansi_term::Colour::{Cyan, Red, Green};
 use std::sync::atomic::{AtomicBool, Ordering::SeqCst};
 
 static RELEASE: AtomicBool = AtomicBool::new(false);
@@ -13,9 +14,9 @@ static DOC: AtomicBool = AtomicBool::new(false);
 fn main() {
 
     WalkDir::new({
-        let app = App::new("cargo-rclean")
+        let args = App::new("cargo-rclean")
             .version("1.0.0")
-            .author("Ares.T <coldswind@pm.me>")
+            .author("Ares <coldswind@pm.me>")
             .about("Clean your rust project, recursively")
             .arg(Arg::with_name("INPUT")
                 .help("Cleans up all rust projects in the specified directory")
@@ -37,15 +38,16 @@ fn main() {
             )
             .get_matches();
 
-        if app.is_present("release") {
+        if args.is_present("release") {
             RELEASE.store(true, SeqCst)
         }
 
-        if app.is_present("doc") {
+        if args.is_present("doc") {
             DOC.store(true, SeqCst)
         }
 
-        app.value_of("INPUT").unwrap().to_string()
+        args.value_of("INPUT").unwrap().to_string()
+
     })
         .into_iter()
         .filter_entry(|e| strategy(e))
@@ -53,45 +55,42 @@ fn main() {
             if let Ok(entry) = p {
                 Ok(entry)
             } else {
-                bail!("Error: {}", Red.bold().paint(p.unwrap_err().to_string()))
+                eprintln!("{}: {}", ANSI_err(), Red.bold().paint(p.unwrap_err().to_string()));
+                bail!("")
             }
         })
-        .map(Result::unwrap)
-        .for_each(|x: DirEntry| {
-            let ret = fs::read_dir(x.path());
-            if let Ok(read_dir) = ret {
-                read_dir
-                    .map(|x| -> Result<PathBuf> {
-                        if let Ok(entry) = x.as_ref() {
-                            Ok(entry.path())
-                        } else {
-                            bail!("{}", Red.bold().paint(x.unwrap_err().to_string()));
-                        }
-                    })
-                    .map(Result::unwrap)
-                    .filter(|x| {
-                        x.is_dir()
-                    })
-                    .filter(|x| {
-                        x.join("Cargo.toml").exists()
-                    })
-                    .filter(|x| {
-                        ["examples", "tests", "benches"]
-                            .iter()
-                            .fold(true, |tag, p| tag & !x.join(p).exists())
-                    })
-                    .filter(|x| {
-                        x.join("target").is_dir()
-                    })
-                    .for_each(|x| {
-                        cargo_clean(x).unwrap_or_else(|e| eprintln!("{}", e));
-                    })
-            } else {
-                eprintln!(
-                    "{}: {}",
-                    Red.bold().paint(ret.unwrap_err().to_string()),
-                    Cyan.bold().underline().paint(path_to_str(x.path()))
-                );
+        .for_each(|x: Result<DirEntry>| {
+            if x.is_ok() {
+                let x = x.unwrap();
+                let ret = fs::read_dir(x.path());
+                if let Ok(read_dir) = ret {
+                    read_dir
+                        .map(|x| -> Result<PathBuf> {
+                            if let Ok(entry) = x.as_ref() {
+                                Ok(entry.path())
+                            } else {
+                                bail!("{}", Red.bold().paint(x.unwrap_err().to_string()));
+                            }
+                        })
+                        .map(Result::unwrap)
+                        .filter(|x| {
+                            x.is_dir()
+                        })
+                        .filter(|x| {
+                            x.join("Cargo.toml").exists()
+                        })
+                        .filter(|x| {
+                            ["examples", "tests", "benches"]
+                                .iter()
+                                .fold(true, |tag, p| tag & !x.join(p).exists())
+                        })
+                        .filter(|x| {
+                            x.join("target").is_dir()
+                        })
+                        .for_each(|x| {
+                            cargo_clean(x).unwrap_or_else(|e| eprintln!("{}! {}", ANSI_err(), e));
+                        })
+                }
             }
         });
 }
@@ -109,12 +108,13 @@ fn cargo_clean(path: impl AsRef<Path>) -> Result<()> {
         .args(&args)
         .current_dir(path.as_ref())
         .output()?.status.success() {
-        println!("{} at {}", Green.bold().paint("OK"), Cyan.bold().underline().paint(path_to_str(path)));
+        println!("{} at {}", ANSI_ok(), path_display(path_to_str(path)));
     } else {
-        println!("{}! In: {}", Red.bold().paint("Error"), Cyan.bold().underline().paint(path_to_str(path)));
+        println!("{}! In: {}", ANSI_err(), path_display(path_to_str(path)));
     }
 
     Ok(())
+
 }
 
 #[inline(always)]
@@ -127,6 +127,23 @@ fn is_hidden(entry: &DirEntry) -> bool {
 }
 
 #[inline(always)]
+fn path_display<'a>(path: impl AsRef<str> + 'a) -> ANSIGenericString<'a, str> {
+    Cyan.bold().underline().paint(path.as_ref().to_string())
+}
+
+#[inline(always)]
+#[allow(non_snake_case)]
+fn ANSI_ok() -> ANSIGenericString<'static, str> {
+    Green.bold().paint("OK")
+}
+
+#[inline(always)]
+#[allow(non_snake_case)]
+fn ANSI_err() -> ANSIGenericString<'static, str> {
+    Red.bold().paint("Error")
+}
+
+#[inline(always)]
 fn path_to_str(path: impl AsRef<Path>) -> String {
     path.as_ref().to_str().unwrap().to_string()
 }
@@ -136,7 +153,7 @@ fn strategy(entry: &DirEntry) -> bool {
     if cfg!(target_os = "macos") {
         !is_hidden(entry) && entry.path().is_dir() && !(
             if let Some(ext) = entry.path().extension() {
-                ext == "app" || ext == "rtfd"
+                ext == "app"
             } else {
                 false
             }
